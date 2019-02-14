baa25d77c56fb7122823d6860681c07332d0e170
arseniyT <30984464+arseniyT@users.noreply.github.com>
2019-02-15 00:31:44 +0300

    Updates

---
+++ precache-manifest.3e60035facbcf2b96e47c1f7c92b9440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9942684fe6fa8d80e138",
+    "url": "/hw_ts-react-redux-news-api/static/css/main.09a552aa.chunk.css"
+  },
+  {
+    "revision": "9942684fe6fa8d80e138",
+    "url": "/hw_ts-react-redux-news-api/static/js/main.9942684f.chunk.js"
+  },
+  {
+    "revision": "479fc1cd3757edfb8c1d",
+    "url": "/hw_ts-react-redux-news-api/static/js/1.479fc1cd.chunk.js"
+  },
+  {
+    "revision": "3f3b5c09f4ad3d22e15a",
+    "url": "/hw_ts-react-redux-news-api/static/js/runtime~main.3f3b5c09.js"
+  },
+  {
+    "revision": "9a266c9d62de5436347fa1ec16ccbf36",
+    "url": "/hw_ts-react-redux-news-api/index.html"
+  }
+];
